865bf932a86ed1a649ecc74db1c2a48706253296
Daniel Tinivella <dtinivella@gmail.com>
2023-01-22 09:49:45 -0300

    add Rocket.toml and configuration port and other thing

--- src/main.rs
+++ src/main.rs
@@ -2,15 +2,16 @@
 extern crate rocket;
 
 
-use rocket::{Build, Rocket};
+// use rocket::{Build, Rocket};
 
 #[get("/")]
-fn index() -> &'static str {
+async fn index() -> &'static str {
 		"Hello, Rocket!"
 }
 
 
-#[launch]
-fn rocket() -> Rocket<Build> {
-		rocket::build().mount("/", routes![index])
+#[rocket::main]
+ async fn main() {
+		rocket::build().mount("/", routes![
+			index]).launch().await;
 }
